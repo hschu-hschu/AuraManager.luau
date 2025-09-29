b79ac84e45e84fd22278593b8b056ed8b89e481a
hschu-hschu <127948183+hschu-hschu@users.noreply.github.com>
2025-09-30 01:14:13 +0900

    Delete AuraManager directory

--- AuraManager/src/ServerScriptService/EquipScript.server.lua
+++
@@ -1,9 +0,0 @@
-local AuraManager = require(game.ServerStorage.Modules.AuraManager)
-
-local AuraName = script:GetAttribute("AuraName")
-
-game.Players.PlayerAdded:Connect(function(player: Player)
-	player.CharacterAdded:Connect(function(character: Model)
-		AuraManager.EquipAura(character,AuraName)
-	end)
-end)
